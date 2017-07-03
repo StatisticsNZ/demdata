1c0b72265d302edab91046e92d1f42a2060b0549
John Bryant <john.r.bryant@gmail.com>
2017-07-03 17:56:38 +1200

    fixed russia.births

--- R/russia.births.R
+++ R/russia.births.R
@@ -1,10 +1,12 @@
 
 #' Births in the Russian Federation by Sex of Child, Age of Mother, and Year.
 #' 
-#' The data come from birth registrations, but refer to the year the birth was occurred, not the year it was registered.  
+#' The data come from birth registrations, but refer to the year the birth was occurred, not the year it was registered.
+#'
+#' The "sex" column gives the sex of the child, and the "age" column gives the age of the child.
 #'
 #'
-#' @format An data.frame with columns "year", "sex", "age.of.mother", and "count".
+#' @format An data.frame with columns "year", "sex", "age", and "count".
 #'
 #' @source United Nations Statistics Division online database 'UNSD Demographic Statistics'.  Downloaded 5 March 2016.
 "russia.births"
--- data-raw/russia.births/russia.births.R
+++ data-raw/russia.births/russia.births.R
@@ -3,7 +3,8 @@ library(dplyr)
 
 russia.births <- read.csv("data-raw/russia.births/UNdata_Export_20160304_232915853.csv",
                           nrows = 410) %>%
-    select(year = Year, sex = Sex, age.of.mother = Age.of.mother, count = Value)
+    select(year = Year, sex = Sex, age = Age.of.mother, count = Value) %>%
+    mutate(age = recode(age, "50 +" = "50+"))
 
 save(russia.births,
      file = "data/russia.births.rda")
